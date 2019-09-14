6a9893534a793996c0cf18116eaa3846e63192e4
Lucas Boulle <mr.boullefigueiredo@gmail.com>
2019-09-13 21:48:34 -0300

    Format code and remove unnecessary attributes

--- src/com/console/Main.java
+++ src/com/console/Main.java
@@ -3,18 +3,21 @@ package com.console;
 import com.console.states.EnumConsoleState;
 import com.console.states.StateMachine;
 
+import java.io.IOException;
+
 import static com.dao.EmployeeRepository.loggedEmployee;
 
 public class Main {
     public static StateMachine consoleState;
     public static void main(String[] args) {
         consoleState =  EnumConsoleState.WELCOME.getMachineState();
-        Boolean exit = false;
+        boolean exit = false;
         while (!exit){
             if(loggedEmployee != null) {
                 System.out.println("Logged in as " + loggedEmployee.getName());
             }
             exit = consoleState.execute();
+            System.out.println("***************");
         }
     }
 }
